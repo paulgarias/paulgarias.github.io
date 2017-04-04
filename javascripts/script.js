2368293339937a8a553a87a7ba03228a89fc6acd
ariaspaul <ariaspaul@gmail.com>
2017-04-04 14:13:17 -0400

    Made enhancements to look of website.

---
+++ javascripts/script.js
@@ -0,0 +1,4 @@
+
+
+
+document.getElementById("profileAndNavigation").innerHTML = '<img src=\"images/profile.jpg\" style=\"width:160px ; box-shadow: 0px 0px 5px #888888;\"> <h1 style=\"text-shadow: -1px 1px 5px #888888;\">PaulGArias</h1> <p>Sharing my adventures in computing and data science since 2014</p> <p>  <iframe src=\"http://ghbtns.com/github-btn.html?user=paulgarias&amp;type=follow\" allowtransparency=\"true\" frameborder=\"0\" scrolling=\"0\" width=\"172px\" height=\"28px\"></iframe></p> <p><a href=\"./index.html\">Home</a></p> <p><a href=\"./about.html\">About</a></p> '
