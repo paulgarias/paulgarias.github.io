b577e3b330a83779d597413231a020d00d18bb61
ariaspaul <ariaspaul@gmail.com>
2017-04-09 09:48:21 -0400

    Added transition timing to links.

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,25 +1,31 @@
-
-var makeHeader = function(){
+(function(){
 	menuLinks = [{"name":"Home","hyperlink":"http://paulgarias.github.io/index.html"},{"name":"About","hyperlink":"http://paulgarias.github.io/about.html"}];
 	imgsrc = "http://paulgarias.github.io/images/profile.jpg";
 	var innerHTMLtext = "";
-	innerHTMLtext = innerHTMLtext+'<img src=\"'+imgsrc+'\"'
-	innerHTMLtext = innerHTMLtext+' style=\"width:160px ; box-shadow: 0px 0px 5px #888888;\">'
-	innerHTMLtext = innerHTMLtext+'  <h1 style=\"text-shadow: -1px 1px 5px #888888;\">PaulGArias</h1> <p>Sharing my adventures in computing and data science since 2014</p> <p> ' 
-	innerHTMLtext = innerHTMLtext+'<a href="http://github.com/paulgarias"> <img class="socmed" src="http://paulgarias.github.io/images/socialmediaicons/github-9-xxl.png" /> </a> '
-	innerHTMLtext = innerHTMLtext+'<a href="http://twitter.com/paulgarias"> <img class="socmed" src="http://paulgarias.github.io/images/socialmediaicons/twitter-4-xxl.png" /> </a> '
-	innerHTMLtext = innerHTMLtext+'</p>'
+	innerHTMLtext += '<img src=\"'+imgsrc+'\"';
+	innerHTMLtext += ' style=\"width:160px ; box-shadow: 0px 0px 5px #888888;\">';
+	innerHTMLtext += '  <h1 style=\"text-shadow: -1px 1px 5px #888888;\">PaulGArias</h1> <p>Sharing my adventures in computing and data science since 2014</p> <p> ';
+	innerHTMLtext += '<a href="http://github.com/paulgarias"><img class="socmed" src="http://paulgarias.github.io/images/socialmediaicons/github-9-xxl.png" /></a> ';
+	innerHTMLtext += '<a href="http://twitter.com/paulgarias"><img class="socmed" src="http://paulgarias.github.io/images/socialmediaicons/twitter-4-xxl.png" /></a> ';
+	innerHTMLtext += '</p>';
 	for (var i=0; i < menuLinks.length ; i++) {
-		innerHTMLtext = innerHTMLtext + '<p><a href=\"'+menuLinks[i]['hyperlink'] + '\">' + menuLinks[i]['name'] + '</a></p>'
+		innerHTMLtext += '<p><a href=\"'+menuLinks[i]['hyperlink'] + '\">' + menuLinks[i]['name'] + '</a></p>';
 	}
-	return innerHTMLtext
-};
-
-document.querySelectorAll("#profileAndNavigation")[0].innerHTML = makeHeader();
+	document.querySelectorAll("#profileAndNavigation")[0].innerHTML = innerHTMLtext;
+})();
 
 (function () {
-	var socmedicons = document.querySelectorAll(".socmed")
+	var socmedicons = document.querySelectorAll(".socmed");
 	for (var i=0; i<socmedicons.length; i++) {
 		socmedicons[i].style.transition="all 0.25s";
 	}
+})();
+
+(function () {
+	var linklist = document.querySelectorAll("a");
+	for (var i=0;i<linklist.length; i++){
+		linklist[i].style.transition = "all 0.25s";
+		console.log(linklist[i].innerText=="");
+	}
+	
 })();
